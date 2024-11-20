1d586a5e17f3075cd58fab5a8b2ba78634746405
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2024-11-20 19:02:57 +0700

    Completing the navbar section

--- src/js/script.js
+++ src/js/script.js
@@ -12,7 +12,9 @@ window.onscroll = function () {
 
 // Hamburger
 const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
